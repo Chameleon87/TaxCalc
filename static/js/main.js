366753e1628dc0d3fae2f4e13279fc130bf54e73
Jesse Hodge <jessehodge1987@gmail.com>
2015-03-01 11:18:45 -0600

    initial commit

---
+++ static/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+//set variables
+var tax;
+var take_home;
+//function to calulate take home pay.
+var tax_takehome_func = function (value) {
+    if  (value <= 9075) {
+        tax = (((value) * 10) / 100);
+        take_home = (value - tax);
+    }
+    else if (value >= 9076 && value <= 36900) {
+        tax = (((value) * 15) / 100);
+        take_home = (value - tax);
+    }
+    else if (value >= 36901 && value <= 89350) {
+        tax = (((value)* 25) / 100);
+        take_home = (value - tax);
+    }
+    else if (value >= 89351 && value <= 186350) {
+        tax = (((value)* 28) / 100);
+        take_home = (value - tax);
+    }
+    else if (value >= 186351 && value <= 405100) {
+        tax = (((value) * 33) / 100);
+        take_home = (value - tax);
+    }
+    else if (value >= 405101 && value <= 406750) {
+        tax = (((value)*35)/100);
+        take_home = (value - tax);
+    }
+    else {
+        tax = (((value)*39.6)/100);
+        take_home = (value - tax);
+    }
+};
+
+$("#salary").keyup(function() {
+    var value = $(this).val();
+
+    tax_takehome_func(value);
+    take_home = 
+    $('#Wages').text("Wages: $" + value);
+    $('#tax').text("Tax: $" + tax);
+    $('#take-home').text("Take Home: $" + (value - tax))
+    $('#monthly-take-home').text("Monthly Take Home: $" + (value - tax)/12)
+    $('#hourly').text("You make: $" + (((value - tax)/12)/40) + " an hour, in a 40 hour work week.")
+});
+});
